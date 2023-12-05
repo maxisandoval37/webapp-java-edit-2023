4024ad1106aaa48ae9d238f05a25d2eab099ab57
Maxi Sandoval <maxisandoval98@gmail.com>
2023-12-04 21:39:42 -0300

    commit inicial

---
+++ src/test/java/com/example/webappjavaedit2023/WebappJavaEdIt2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webappjavaedit2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappJavaEdIt2023ApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}

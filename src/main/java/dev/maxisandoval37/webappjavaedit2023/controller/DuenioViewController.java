3b4325e9f720cdc54822b5aeea9318e7693097bc
Maxi Sandoval <maxisandoval98@gmail.com>
2023-12-11 22:09:08 -0300

    solucion bug vistas no encontradas

--- src/main/java/dev/maxisandoval37/webappjavaedit2023/controller/DuenioViewController.java
+++ src/main/java/dev/maxisandoval37/webappjavaedit2023/controller/DuenioViewController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
@@ -21,7 +20,7 @@ public class DuenioViewController {
     public String listarDuenios(Model model){
         List<Duenio> duenios = duenioService.listarDuenios();
         model.addAttribute("duenios", duenios);
-        return "duenio";
+        return "duenios";
     }
 
     @GetMapping("/agregarDuenio")

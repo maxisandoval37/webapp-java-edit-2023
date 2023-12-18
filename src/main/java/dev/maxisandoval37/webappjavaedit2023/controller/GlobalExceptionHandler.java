f6aabb1483e1cd33a101e09df3ed1159a34ec0e0
Maxi Sandoval <maxisandoval98@gmail.com>
2023-12-18 19:15:10 -0300

    agregada pagina de error

---
+++ src/main/java/dev/maxisandoval37/webappjavaedit2023/controller/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package dev.maxisandoval37.webappjavaedit2023.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e, Model model) {
+        // Puedes personalizar el manejo de excepciones según tus necesidades
+        model.addAttribute("error", e.getMessage());
+        return "error";
+    }
+}
